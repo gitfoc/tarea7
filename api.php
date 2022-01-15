e6847fcc986be35125e96d3bc3524da64b8f83ee
gitfoc <39069790+gitfoc@users.noreply.github.com>
2022-01-15 18:09:43 +0100

    Update api.php

--- api.php
+++ api.php
@@ -15,12 +15,23 @@ function get_datos_autor($id){
     $info_autor = array();
     //Esta información se cargará de la base de datos
     switch ($id){
+		//IMPORTANTE: Este bloque switch simula dos consultas de la base de datos, 
+		// la de libros será un join de las dos tablas
         case 0:
-          $info_autor = array("nombre" => "J. R. R.", "apellidos" => "Tolkien", "nacionalidad" => "Inglaterra"); 
-          break;
+          $info_autor["datos"] = array("nombre" => "J. R. R.", "apellidos" => "Tolkien", "nacionalidad" => "Inglaterra"); 
+          $info_autor["libros"] = array(
+			array("id" => 0, "titulo" =>"El Hobbit", "f_publicacion" =>"21/09/1937")
+			,array("id" => 1, "titulo" =>"La Comunidad del Anillo", "f_publicacion" =>"29/07/1954")
+			,array("id" => 2, "titulo" =>"Las dos torres", "f_publicacion" =>"11/11/1954")
+			,array("id" => 3, "titulo" =>"El retorno del Rey", "f_publicacion" =>"20/10/1955"));		  
+		  break;
         case 1:
-          $info_autor = array("nombre" => "Isaac", "apellidos" => "Asimov", "nacionalidad" => "Rusia"); 
-          break;
+          $info_autor["datos"] = array("nombre" => "Isaac", "apellidos" => "Asimov", "nacionalidad" => "Rusia"); 
+          $info_autor["libros"] = array(
+			array("id" => 4, "titulo" =>"Un guijarro en el cielo", "f_publicacion" =>"19/01/1950")
+			,array("id" => 5, "titulo" =>"Fundación", "f_publicacion" =>"01/06/1951")
+			,array("id" => 6, "titulo" =>"Yo, robot", "f_publicacion" =>"02/12/1950"));
+		  break;
     }
     
     return $info_autor;
