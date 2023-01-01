6c95292bc34b38eae6a591ad06ad7e753c29673e
gitfoc <39069790+gitfoc@users.noreply.github.com>
2023-01-01 23:39:53 +0100

    Update api.php

--- api.php
+++ api.php
@@ -2,36 +2,47 @@
 // Esta API tiene dos posibilidades; Mostrar una lista de autores o mostrar la información de un autor específico.
 
 function get_lista_autores(){
+	//Datos de la aplicación, simulando datos en bd
+	$obj = new stdClass();
+	$obj->id="0";
+	$obj->nombre="J. R. R.";
+	$obj->apellidos="Tolkien";
+	$obj->nacionalidad="Inglaterra";
+	$obj1 = new stdClass();
+	$obj1->id="2";
+	$obj1->nombre="Isaac";
+	$obj1->apellidos="Asimov";	
+	$obj1->nacionalidad="Rusia";
+
     //Esta información se cargará de la base de datos
-    $lista_autores = array(
-        array("id" => 0, "nombre" => "J. R. R.", "apellidos" => "Tolkien" ),
-        array("id" => 1, "nombre" => "Isaac", "apellidos" => "Asimov")
-    );
+
+	$lista_autores = array($obj, $obj1);
     
     return $lista_autores;
 }
 
 function get_datos_autor($id){
+	//Datos de la aplicación, simulando datos en bd
+	$obj = new stdClass();
+	$obj->id="0";
+	$obj->nombre="J. R. R.";
+	$obj->apellidos="Tolkien";
+	$obj->nacionalidad="Inglaterra";
+	$obj1 = new stdClass();
+	$obj1->id="2";
+	$obj1->nombre="Isaac";
+	$obj1->apellidos="Asimov";	
+	$obj1->nacionalidad="Rusia";
+	
     $info_autor = array();
     //Esta información se cargará de la base de datos
     switch ($id){
-		//IMPORTANTE: Este bloque switch simula dos consultas de la base de datos, 
-		// la de libros será un join de las dos tablas
         case 0:
-          $info_autor["datos"] = array("nombre" => "J. R. R.", "apellidos" => "Tolkien", "nacionalidad" => "Inglaterra"); 
-          $info_autor["libros"] = array(
-			array("id" => 0, "titulo" =>"El Hobbit", "f_publicacion" =>"21/09/1937")
-			,array("id" => 1, "titulo" =>"La Comunidad del Anillo", "f_publicacion" =>"29/07/1954")
-			,array("id" => 2, "titulo" =>"Las dos torres", "f_publicacion" =>"11/11/1954")
-			,array("id" => 3, "titulo" =>"El retorno del Rey", "f_publicacion" =>"20/10/1955"));		  
-		  break;
+          $info_autor = array($obj); 
+          break;
         case 1:
-          $info_autor["datos"] = array("nombre" => "Isaac", "apellidos" => "Asimov", "nacionalidad" => "Rusia"); 
-          $info_autor["libros"] = array(
-			array("id" => 4, "titulo" =>"Un guijarro en el cielo", "f_publicacion" =>"19/01/1950")
-			,array("id" => 5, "titulo" =>"Fundación", "f_publicacion" =>"01/06/1951")
-			,array("id" => 6, "titulo" =>"Yo, robot", "f_publicacion" =>"02/12/1950"));
-		  break;
+          $info_autor = array($obj1); 
+          break;
     }
     
     return $info_autor;
